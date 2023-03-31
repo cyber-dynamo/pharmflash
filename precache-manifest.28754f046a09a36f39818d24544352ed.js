d7cb2479d2b13ff9fcbe0f8a32c74a57abb40902
Paco Chan <pacochan@Pacos-MacBook-Air.local>
2023-03-31 14:16:21 -0700

    Updates

---
+++ precache-manifest.28754f046a09a36f39818d24544352ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "386b83dc4bd4445f28e3",
+    "url": "/pharmflash/static/css/main.63eac327.chunk.css"
+  },
+  {
+    "revision": "386b83dc4bd4445f28e3",
+    "url": "/pharmflash/static/js/main.386b83dc.chunk.js"
+  },
+  {
+    "revision": "82f3f0cc799277e00c27",
+    "url": "/pharmflash/static/js/1.82f3f0cc.chunk.js"
+  },
+  {
+    "revision": "69721acfc6b47df0c60b",
+    "url": "/pharmflash/static/css/2.e0556dbd.chunk.css"
+  },
+  {
+    "revision": "69721acfc6b47df0c60b",
+    "url": "/pharmflash/static/js/2.69721acf.chunk.js"
+  },
+  {
+    "revision": "672e99577f8a9dd903c3",
+    "url": "/pharmflash/static/js/runtime~main.672e9957.js"
+  },
+  {
+    "revision": "3902c4968f4e161153a10e962cdf9383",
+    "url": "/pharmflash/index.html"
+  }
+];
